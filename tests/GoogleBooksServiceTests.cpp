d22354c22eebb4d2c67ca1dcbdae4f04f872c44d
pepipe <pepipee@gmail.com>
2025-01-22 03:30:16 +0000

    add more tests

---
+++ tests/GoogleBooksServiceTests.cpp
@@ -0,0 +1,40 @@
+#include <GoogleBooksService.h>
+#include <gtest/gtest.h>
+
+
+class GoogleBooksServiceTests : public ::testing::Test {
+protected:
+    GoogleBooksService service;
+};
+
+TEST_F(GoogleBooksServiceTests, AddToFavoritesAddsABookToFavorites)
+{
+    // Arrange
+    std::string bookId = "book1";
+    std::string bookJson = "{\"title\": \"Book 1\"}";
+
+    // Act
+    service.AddToFavorites(bookId, bookJson);
+
+    // Assert
+    EXPECT_TRUE(service.IsFavorite(bookId));
+}
+
+TEST_F(GoogleBooksServiceTests, GetFavoriteBooksRetrievesFavoriteBooksCorrectly)
+{
+    // Arrange
+    std::string bookId1 = "book1";
+    std::string bookJson1 = "{\"title\": \"Book 1\"}";
+    std::string bookId2 = "book2";
+    std::string bookJson2 = "{\"title\": \"Book 2\"}";
+
+    // Act
+    service.AddToFavorites(bookId1, bookJson1);
+    service.AddToFavorites(bookId2, bookJson2);
+    std::vector<std::string> favorites = service.GetFavoriteBooks();
+
+    // Assert
+    EXPECT_EQ(favorites.size(), 2);
+    EXPECT_NE(std::find(favorites.begin(), favorites.end(), bookJson1), favorites.end());
+    EXPECT_NE(std::find(favorites.begin(), favorites.end(), bookJson2), favorites.end());
+}
