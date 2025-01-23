b5761a95ffb3a63facdaa6086aa8e25d486927c5
pepipe <pepipee@gmail.com>
2025-01-23 02:49:07 +0000

    Removed async method because it makes app crash. Check feat/asyncFetchBook branch

--- src/jni/GoogleBooks_JNI.cpp
+++ src/jni/GoogleBooks_JNI.cpp
@@ -8,53 +8,6 @@ static GoogleBooksService googleBooksService;
 
 extern "C"
 {
-JNIEXPORT void JNICALL Java_com_example_booksclient_services_GoogleBooksService_fetchBooksAsync
-(JNIEnv *env, jclass clazz, jobject callbackObj, jstring jQuery, jint startIndex, jint maxResults)
-{
-    // Convert the Java string to a C++ string
-    const char *queryChars = env->GetStringUTFChars(jQuery, nullptr);
-    std::string queryStr(queryChars);
-    env->ReleaseStringUTFChars(jQuery, queryChars);
-
-    std::thread([env, callbackObj, queryStr, startIndex, maxResults]()
-    {
-        JavaVM *jvm;
-        env->GetJavaVM(&jvm);
-        JNIEnv *localEnv;
-        jvm->AttachCurrentThread(reinterpret_cast<void **>(&localEnv), nullptr);
-
-        std::string result;
-        try
-        {
-            googleBooksService.FetchBooks(queryStr, startIndex, maxResults,
-                                          [&](const std::string &json, int, const std::string &)
-                                          {
-                                              result = json;
-                                          });
-
-            // Get Java class and method
-            jclass callbackClass = localEnv->GetObjectClass(callbackObj);
-            jmethodID callbackMethod = localEnv->GetMethodID(callbackClass, "onResult", "(Ljava/lang/String;)V");
-
-            // Create new UTF-16 java string from UTF-8 c++ string
-            jbyteArray bytes = env->NewByteArray(result.length());
-            env->SetByteArrayRegion(bytes, 0, result.length(), reinterpret_cast<const jbyte *>(result.c_str()));
-            jstring encoding = env->NewStringUTF("UTF-8");
-            jclass classString = env->FindClass("java/lang/String");
-            jmethodID ctorString = env->GetMethodID(classString, "<init>", "([BLjava/lang/String;)V");
-            jstring resultJStr = jstring(env->NewObject(classString, ctorString, bytes, encoding));
-
-            // Invoke the callback method in Java
-            localEnv->CallVoidMethod(callbackObj, callbackMethod, resultJStr);
-        } catch (const std::exception &e)
-        {
-            // Handle any error that occurred and convert it to Java exception if necessary
-        }
-
-        jvm->DetachCurrentThread(); // Detach when done
-    }).detach(); // Detach thread to avoid blocking
-}
-
 JNIEXPORT jstring JNICALL Java_com_example_booksclient_services_GoogleBooksService_fetchBooks
 (JNIEnv *env, jclass clazz, const jstring query,
  const jint startIndex, const jint maxResults)
